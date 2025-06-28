d788b3768f48902a775240182dc6247b47f9bd1f
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-06-29 06:00:57 +0900

    Update Insertion-Sort---Part-2.cpp

--- Insertion-Sort---Part-2.cpp
+++ Insertion-Sort---Part-2.cpp
@@ -1,3 +1,25 @@
+void printArray(const vector<int>& arr) {
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+}
+
+void insertionSort2(int n, vector<int>& arr) {
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+
+        printArray(arr);
+    }
+}
+
 template <typename T>
 void show_value(const T& arr)
 {
