93adbac284d3f42ca050c6b0ce3adb6690fbd398
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-07-04 04:09:00 +0900

    Update Insertion-Sort---Part-2.java

--- Insertion-Sort---Part-2.java
+++ Insertion-Sort---Part-2.java
@@ -1,1 +1,40 @@
-Insertion-Sort---Part-2
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'insertionSort2' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY arr
+     */
+
+    static void showValue(List<Integer> arr) {
+        for (int num : arr) System.out.print(num + " ");
+        System.out.println();
+    }
+
+    public static void insertionSort2(int n, List<Integer> arr) {
+        for (int i = 1; i < n; ++i) {
+            int key = arr.get(i);
+            int j = i - 1;
+            while (j >= 0 && arr.get(j) > key) {
+                arr.set(j + 1, arr.get(j));
+                --j;
+            }
+            arr.set(j + 1, key);
+            showValue(arr);
+        }
+    }    
+}
